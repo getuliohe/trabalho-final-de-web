6ec430230305a8f51c64c1f8d25d9aa06f135e30
Getulio Lima <getuliofurtado03@gmail.com>
2024-08-08 16:44:43 -0300

    funcionalidade create do character funcionando

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,23 @@ const ageInputChar = document.querySelector("#ageInputChar");
 const raceInputChar = document.querySelector("#raceInputChar");
 const classInputChar = document.querySelector("#classInputChar");
 
+let characters = JSON.parse(localStorage.getItem('characters')) || []
+if (!Array.isArray(characters)){
+    characters = [characters]
+}
+
 formularioPersonagem.addEventListener('submit', (e) => {
+
     e.preventDefault()
-    
+    if(raceInputChar.value == "-----" || classInputChar.value == "-----"){
+        alert("Selecione uma opção em raça ou classe!");
+        return
+    }
     let myCharacter = new Character(nameInputChar.value, ageInputChar.value, raceInputChar.value, classInputChar.value);
 
-    console.log(myCharacter)
+    if(myCharacter){
+        characters.push(myCharacter);
+        localStorage.setItem("characters",JSON.stringify(characters));
+    }
+
 })
---
+++ scripts/mountPet.js
@@ -0,0 +1,71 @@
+export class Character {
+    constructor(name, age, race, role) {
+        this.name = name;
+        this.age = age;
+        this.race = race;
+        this.dexterity = 0;
+        this.strength = 0;
+        this.charisma = 0;
+        this.wisdom = 0;
+        this.intelligence = 0;
+        this.constitution = 0;
+
+        this.applyModifiers();
+    }
+
+    applyModifiers() {
+        if (this.race.toLowerCase() === "wolf") {
+            this.wisdom += 1;
+            this.strength += 1;
+            this.dexterity += 1;
+            this.constitution += 1;
+            this.intelligence += 1;
+            this.charisma += 1;
+        } else if (this.race.toLowerCase() === "young dragon") {
+            this.wisdom += 1;
+            this.dexterity += 1;
+            this.intelligence += 1;
+            this.charisma += 2;
+        } else if (this.race.toLowerCase() === "dwarf") {
+            this.constitution += 2;
+            this.strength += 2;
+            this.wisdom += 1;
+        } else if (this.race.toLowerCase() === "halfling") {
+            this.dexterity += 2;
+            this.constitution += 1;
+            this.charisma += 1;
+        }
+
+        if (this.role.toLowerCase() === "mage") {
+            this.wisdom += 2;
+            this.intelligence += 1;
+        } else if (this.role.toLowerCase() === "barbarian") {
+            this.strength += 3;
+            this.constitution += 1;
+        } else if (this.role.toLowerCase() === "cleric") {
+            this.charisma += 2;
+            this.wisdom += 1;
+        } else if (this.role.toLowerCase() === "ranger") {
+            this.dexterity += 2;
+            this.strength += 1;
+        } else if (this.role.toLowerCase() === "druid") {
+            this.intelligence += 2;
+            this.wisdom += 1;
+        } else if (this.role.toLowerCase() === "monk") {
+            this.dexterity += 2;
+            this.strength += 2;
+        } else if (this.role.toLowerCase() === "sorcerer") {
+            this.charisma += 2;
+            this.constitution += 1;
+        } else if (this.role.toLowerCase() === "warrior") {
+            this.strength += 1;
+            this.constitution += 3;
+        } else if (this.role.toLowerCase() === "paladin") {
+            this.charisma += 2;
+            this.wisdom += 1;
+        } else if (this.role.toLowerCase() === "rogue") {
+            this.dexterity += 3;
+            this.intelligence += 1;
+        }
+    }
+}
