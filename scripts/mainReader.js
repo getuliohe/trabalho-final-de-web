d1df59e00fddac35f8ded4f173eb274052027ccc
Getulio Lima <getuliofurtado03@gmail.com>
2024-08-09 13:26:24 -0300

    edit pronto

--- scripts/mainReader.js
+++ scripts/mainReader.js
@@ -1,38 +1,56 @@
 const charContainer = document.querySelector(".charContainer");
 const petContainer = document.querySelector(".petContainer");
 
-// Recupera e assegura que 'pets' seja um array
 let pets = JSON.parse(localStorage.getItem('pets')) || [];
 if (!Array.isArray(pets)) {
     pets = [pets];
 }
 
-// Recupera e assegura que 'characters' seja um array
 let characters = JSON.parse(localStorage.getItem('characters')) || [];
 if (!Array.isArray(characters)) {
     characters = [characters];
 }
 
-// Função para exibir personagens
 function displayCharacters(characters) {
-    charContainer.innerHTML = ''; // Limpa o container antes de adicionar novos elementos
+    charContainer.innerHTML = '';
 
     characters.forEach(character => {
         const charElement = document.createElement('div');
+        const editButton = document.createElement('button')
+
+        editButton.onclick = () => {
+            const name = prompt("Digite o nome: ");
+            const age = prompt("Digite a idade: ");
+            let race = "";
+            let role = "";
+
+            while(race != "human" && race != "elf" && race != "dwarf" && race != "halfling" ){
+                race = prompt("Digite a raça (human;elf;dwarf;halfling): ");
+            }            
+            while(role != "mage" && role != "barbarian" && role != "cleric" && role != "ranger" && role != "druid" && role != "monk" && role != "sorcerer" && role != "warrior" && role != "paladin" && role != "rogue"){
+                role = prompt("Digite a classe (mage;barbarian;cleric;ranger;druid;monk;sorcerer;warrior;paladin;rogue): ");
+            }            
+            character.name = name;
+            character.age = age;
+            character.race = race;
+            character.role = role;
+            localStorage.setItem("characters", JSON.stringify(characters))
+            displayCharacters(characters)
+        }
         charElement.className = 'character-item';
         charElement.innerHTML = `
             <h3>${character.name}</h3>
             <p>Idade: ${character.age}</p>
             <p>Raça: ${character.race}</p>
-            <p>Classe: ${character.class}</p>
+            <p>Classe: ${character.role}</p>
         `;
         charContainer.appendChild(charElement);
+        charContainer.appendChild(editButton)
     });
 }
 
-// Função para exibir pets
 function displayPets(pets) {
-    petContainer.innerHTML = ''; // Limpa o container antes de adicionar novos elementos
+    petContainer.innerHTML = '';
 
     pets.forEach(pet => {
         const petElement = document.createElement('div');
@@ -42,10 +60,25 @@ function displayPets(pets) {
             <p>Raça: ${pet.race}</p>
             <p>Idade: ${pet.age}</p>
         `;
+        const editButton = document.createElement('button')
+
+        editButton.onclick = () => {
+            const name = prompt("Digite o nome: ");
+            const age = prompt("Digite a idade: ");
+            let race = "";
+            while(race != "wolf" && race != "young dragon" && race != "giant frog" && race != "stormy hawk" ){
+                race = prompt("Digite a raça (wolf; young dragon; giant frog; stormy hawk): ");
+            }            
+            pet.name = name;
+            pet.age = age;
+            pet.race = race;
+            localStorage.setItem("pets", JSON.stringify(pets))
+        }
+
         petContainer.appendChild(petElement);
+        petContainer.appendChild(editButton)
     });
 }
 
-// Chama as funções para exibir os dados
 displayCharacters(characters);
 displayPets(pets);
